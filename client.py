ec46bcffc7ac9e74c2405ffbd2c30b2892779ee1
m33ls <70194930+ameliiams@users.noreply.github.com>
2022-02-20 09:43:28 -0800

    send message back to client

--- client.py
+++ client.py
@@ -1,15 +1,12 @@
 import socket
-import sys
 
 address = "localhost"
 port = 81
 
 tcp_socket = socket.create_connection((address, port))
 
-try:
-    data = bytes(input("Type a message: "), 'utf-8')
-    tcp_socket.sendall(data)
+data = bytes(input("Type a message: "), 'utf-8')
+tcp_socket.sendall(data)
 
-finally:
-    print("Closing socket...")
-    tcp_socket.close()
+data_from_server = str(tcp_socket.recv(1024))
+print(f"Message from server: {data_from_server}")
--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import socket
-import sys
 
 address = "localhost"
 port = 81
@@ -9,20 +8,14 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (address, port)
 tcp_socket.bind(server_address)
 
-tcp_socket.listen(1)
+tcp_socket.listen(10)
+print("Awaiting connection...")
 
-while True:
-    print("Awaiting connection...")
-    connection, client = tcp_socket.accept()
+connection, client = tcp_socket.accept()
+print(f"Client connection: {client}")
 
-    try:
-        print(f"Client connection: {client}")
-
-        while True:
-            data = connection.recv(32)
-            print(f"Received data: {data}")
-
-            if not data:
-                break
-    finally:
-        connection.close()
+data = connection.recv(1024)
+print(f"Received data: {data}")
+    
+connection.sendall(data)
+connection.close()
